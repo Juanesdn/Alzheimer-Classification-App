b5150e2688995889c46e6faf0183ee236142de46
Juan De La Cruz <jedelacruzn@gmail.com>
2022-04-21 23:07:17 -0500

    first commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,19 @@
+import { Meta } from '@/layout/Meta';
+import { Main } from '@/templates/Main';
+
+const Index = () => {
+  return (
+    <Main
+      meta={
+        <Meta
+          title="Alzheimer Classification App"
+          description="Alzheimer Classification App"
+        />
+      }
+    >
+      <div className="text-center">XD</div>
+    </Main>
+  );
+};
+
+export default Index;
---
+++ src/utils/AppConfig.ts
@@ -0,0 +1,6 @@
+export const AppConfig = {
+  site_name: 'Alzheimer Classification App',
+  title: 'Alzheimer Classification App',
+  description: 'Alzheimer Classification App',
+  locale: 'en',
+};
