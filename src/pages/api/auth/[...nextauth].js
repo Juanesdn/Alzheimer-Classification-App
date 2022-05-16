811ca091a67378d9cc37618d3c4b527ea0a07478
Juan De La Cruz <jedelacruzn@gmail.com>
2022-05-16 10:14:37 -0500

    login con email

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,14 +1,14 @@
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import NextAuth from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
-/* import EmailProvider from 'next-auth/providers/email';
- */
+import EmailProvider from 'next-auth/providers/email';
+
 import clientPromise from '../../../lib/mongodb';
 
 export default NextAuth({
   adapter: MongoDBAdapter(clientPromise),
   providers: [
-    /* EmailProvider({
+    EmailProvider({
       server: {
         host: process.env.EMAIL_SERVER_HOST,
         port: process.env.EMAIL_SERVER_PORT,
@@ -18,7 +18,7 @@ export default NextAuth({
         },
       },
       from: process.env.EMAIL_FROM,
-    }), */
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,4 +1,5 @@
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import EmailIcon from '@mui/icons-material/Email';
 import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import {
   Avatar,
@@ -6,6 +7,7 @@ import {
   Grid,
   IconButton,
   Paper,
+  TextField,
   Typography,
 } from '@mui/material';
 import { BuiltInProviderType } from 'next-auth/providers';
@@ -15,6 +17,7 @@ import {
   LiteralUnion,
   signIn,
 } from 'next-auth/react';
+import { useState } from 'react';
 import { FcGoogle } from 'react-icons/fc';
 
 import Link from '@/components/Link/Link';
@@ -27,6 +30,7 @@ interface IMainProps {
 }
 
 const SignUp = (props: IMainProps) => {
+  const [email, setEmail] = useState<string>('');
   return (
     <Grid container component="main" sx={{ height: '100vh' }}>
       <Grid
@@ -88,29 +92,72 @@ const SignUp = (props: IMainProps) => {
           <Typography component="h1" variant="h5">
             Iniciar Sesión
           </Typography>
-          <Box component="div" sx={{ mt: 1 }}>
-            {Object.values(props.providers).map((provider) => (
-              <div key={provider.name}>
-                <button
-                  className="mt-5"
-                  onClick={() =>
-                    signIn(provider.id, {
-                      callbackUrl: `${window.location.origin}/dashboard`,
-                    })
-                  }
-                >
-                  {/* <svg
+          <Box
+            className="flex flex-col items-center justify-center"
+            component="div"
+            sx={{ mt: 1, width: '100%' }}
+          >
+            <TextField
+              margin="normal"
+              fullWidth
+              id="email"
+              label="Email Address"
+              name="email"
+              autoComplete="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              autoFocus
+            />
+            <button
+              className="flex w-full items-center justify-center rounded-lg bg-indigo-500 px-4 py-2 text-sm text-white hover:border-indigo-700 hover:bg-indigo-700 focus:border-indigo-700"
+              onClick={() =>
+                signIn('email', {
+                  callbackUrl: `${window.location.origin}/dashboard`,
+                  email,
+                })
+              }
+            >
+              {/* <svg
                     className="mr-2 h-4 w-4 fill-current"
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="0 0 20 20"
                   >
                     <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
                   </svg> */}
-                  <FcGoogle className="mr-2 h-6 w-6" />
-                  <span>Iniciar sesión con {provider.name}</span>
-                </button>
-              </div>
-            ))}
+              <EmailIcon className="mr-2 h-6 w-6" />
+              <span>Iniciar sesión con email</span>
+            </button>
+            <div className="relative flex w-full items-center py-5">
+              <div className="grow border-t border-gray-400"></div>
+              <span className="mx-4 shrink text-gray-400">o</span>
+              <div className="grow border-t border-gray-400"></div>
+            </div>
+
+            {Object.values(props.providers).map((provider) => {
+              return (
+                provider.name !== 'Email' && (
+                  <button
+                    key={provider.name}
+                    className="flex w-full items-center justify-center rounded-lg border border-gray-300 bg-white px-4 py-2 text-sm text-gray-700 hover:border-gray-500 focus:border-gray-500"
+                    onClick={() =>
+                      signIn(provider.id, {
+                        callbackUrl: `${window.location.origin}/dashboard`,
+                      })
+                    }
+                  >
+                    {/* <svg
+                    className="mr-2 h-4 w-4 fill-current"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 20 20"
+                  >
+                    <path d="M13 8V2H7v6H2l8 8 8-8h-5zM0 18h20v2H0v-2z" />
+                  </svg> */}
+                    <FcGoogle className="mr-2 h-6 w-6" />
+                    <span>Iniciar sesión con {provider.name}</span>
+                  </button>
+                )
+              );
+            })}
           </Box>
         </Box>
       </Grid>
